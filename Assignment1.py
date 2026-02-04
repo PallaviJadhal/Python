44daadbb61744f6e1fe0c7c4b88aa582b0942c12
Pallavi Jadhal <pallavijadhal00@gmail.com>
2026-02-04 19:45:02 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,11 @@
+# print name 
+print("1st")
+print("Pallavi Jadhal")
+
+# print name 10 times 
+print("2nd")
+print("Pallavi Jadhal"*10)
+
+# new line
+print("3rd")
+print(("Pallavi Jadhal"+"\n")*10)
